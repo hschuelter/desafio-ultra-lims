ccc3a9bce7b80500aac284c5895653744133076d
Arthur <arthur.schuelter@gmail.com>
2023-11-13 16:30:36 -0300

    [ultra-lims] fixing database

--- server/index.js
+++ server/index.js
@@ -1,15 +1,7 @@
 const express = require("express");
 const path = require('path');
 const bodyParser = require('body-parser')
-const { Pool } = require('pg');
-
 let cors = require("cors");
-
-const connectStr = process.env.DATABASE_URL;
-const pool = new Pool({
-    connectionString: connectStr,
-    ssl: true
-});
 
 const PORT = process.env.PORT || 3001;
 
@@ -41,21 +33,7 @@ var data = {
 app.use(cors());
 
 app.get("/api/enderecos", (req, res) => {
-    exports.dbAction = function(req, res) {
-
-        process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-        
-        pool.query("SELECT * FROM ENDERECOS;", (err, 
-        results) => {
-                if (err) {
-                    console.log(err);
-                    throw err;
-                }
-            console.log(results.rows);
-            res.json(results.rows);
-            });
-          
-        }
+    res.json(data);
 });
 
 app.post("/api/post", (req, res) => {
